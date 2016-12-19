8be9d7daa7f5bd32d89e463f650baeb0581f8f62
nischal <nischal.bhatewara@gmail.com>
2016-12-19 22:28:58 +0530

    Added explanation to code

--- drawing_book.py
+++ drawing_book.py
@@ -1,3 +1,8 @@
+# Solution to Drawing Book (https://www.hackerrank.com/contests/w27/challenges/drawing-book)
+# We start off by checking if the page to flip to (p) is in the first half or second half of the book.
+# if in first half, increment flip counter till right page is greater than p
+# else increment flip counter till left page is lesser than p
+
 n = int(input().strip())
 p = int(input().strip())
 
