933fe2ad3bd8017fdda68f5d9657fd26747a776b
nischal <nischal.bhatewara@gmail.com>
2016-12-19 22:25:50 +0530

    Drawing book complete!
    - Score 10

--- drawing_book.py
+++ drawing_book.py
@@ -7,17 +7,13 @@ if p / n > 0.5:
     front = False
     cur_page = n
 
+flips = 0
 if front:
-    flips = 0
     while p > cur_page:
         flips += 1
         cur_page += 2
 else:
-    if n % 2 != 0:
-        flips = -1
-    else:
-        flips = 0
-    while p < cur_page:
+    while p < (cur_page - 1):
         flips += 1
         cur_page -= 2
 
