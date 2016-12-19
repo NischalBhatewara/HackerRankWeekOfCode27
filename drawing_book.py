3a4340d9d5e1e0b6ad7ef76978a1601a2bf7f144
nischal <nischal.bhatewara@gmail.com>
2016-12-19 22:22:44 +0530

    Score 7.06

--- drawing_book.py
+++ drawing_book.py
@@ -1,1 +1,24 @@
-print("Hello")
+n = int(input().strip())
+p = int(input().strip())
+
+front = True
+cur_page = 1
+if p / n > 0.5:
+    front = False
+    cur_page = n
+
+if front:
+    flips = 0
+    while p > cur_page:
+        flips += 1
+        cur_page += 2
+else:
+    if n % 2 != 0:
+        flips = -1
+    else:
+        flips = 0
+    while p < cur_page:
+        flips += 1
+        cur_page -= 2
+
+print(flips)
