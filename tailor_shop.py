8d9b44159ca2b1cfecdf3d5955386f53a66c6d5d
nischal <nischal.bhatewara@gmail.com>
2016-12-20 16:32:15 +0530

    Score 11.11

--- tailor_shop.py
+++ tailor_shop.py
@@ -3,15 +3,20 @@ import math
 n, p = input().strip().split(' ')
 n, p = [int(n), int(p)]
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
-clusters = [math.ceil(ai / p) for ai in a]
 # clusters.sort()
 
 tot_buttons = 0
-button_array = []
-for cluster_size in clusters:
-    if cluster_size in button_array:
-        cluster_size = max(button_array) + 1
-    button_array.append(cluster_size)
-    tot_buttons += cluster_size
+# button_array = []
+# for cluster_size in clusters:
+#     if cluster_size in button_array:
+#         cluster_size = max(button_array) + 1
+#     button_array.append(cluster_size)
+#     tot_buttons += cluster_size
+button_max = 0
+for cluster_cost in a:
+    size = math.ceil(cluster_cost / p)
+    if size <= button_max:
+        size = button_max + 1
+    button_max, tot_buttons = size, tot_buttons + size
 
 print(tot_buttons)
