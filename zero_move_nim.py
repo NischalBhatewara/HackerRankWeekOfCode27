f9d5095276bd5a74beaaca55f09cab098eeecc51
nischal <nischal.bhatewara@gmail.com>
2016-12-23 12:32:02 +0530

    Score 12.5

--- zero_move_nim.py
+++ zero_move_nim.py
@@ -4,4 +4,8 @@ g = int(input().strip())
 for game in range(g):
     n = int(input().strip())
     p = [int(p_temp) for p_temp in input().strip().split(' ')]
-    print(functools.reduce(lambda a, b: a ^ b, p))
+    p_xor = functools.reduce(lambda a, b: a ^ b, p)
+    if p_xor == 0 and n % 2 == 0:
+        print('L')
+    else:
+        print('W')
