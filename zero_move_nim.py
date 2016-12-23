9d606cbcb753fd00f570cc2597055c268bdb38de
nischal <nischal.bhatewara@gmail.com>
2016-12-23 13:10:16 +0530

    No change in score

--- zero_move_nim.py
+++ zero_move_nim.py
@@ -5,7 +5,7 @@ for game in range(g):
     n = int(input().strip())
     p = [int(p_temp) for p_temp in input().strip().split(' ')]
     p_xor = functools.reduce(lambda a, b: a ^ b, p)
-    if p_xor == 0 and n % 2 == 0:
+    if (p_xor == 0 and n % 2 == 0) or (p_xor != 0 and n % 2 != 0):
         print('L')
     else:
         print('W')
