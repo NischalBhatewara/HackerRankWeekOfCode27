bc1ed61ede8031572d8ed35631ac01d7158d3af1
nischal <nischal.bhatewara@gmail.com>
2016-12-24 18:15:54 +0530

    Implementing BSF

--- coprime_paths.py
+++ coprime_paths.py
@@ -2,27 +2,39 @@
 #       2. for each edge btw u, v:
 #           if gcd(u, v) == 1:
 #               coprime_edge += 1
-
-
-def get_path(start, end):
-    return
-
-
 n, q = input().strip().split(' ')
 n, q = [int(n), int(q)]
+
+nodes_seen = [0] * n
+nodes_parent = [-1] * n
 nodes = [int(nodes_temp) for nodes_temp in input().strip().split(' ')]
-edges = [[-1] * n for i in range(n)]
-print(edges)
+edge_matrix = [[-1] * n for i in range(n)]
+
 for edges_i in range(n - 1):
     edges_t = [int(edges_temp) for edges_temp in input().strip().split(' ')]
     # adding vales to edge matrix
-    edges[edges_t[0] - 1][edges_t[1] - 1] = 1
-    edges[edges_t[0] - 1][edges_t[0] - 1] = 0
-    edges[edges_t[1] - 1][edges_t[1] - 1] = 0
-    edges[edges_t[1] - 1][edges_t[0] - 1] = 1
+    edge_matrix[edges_t[0] - 1][edges_t[1] - 1] = 1
+    edge_matrix[edges_t[0] - 1][edges_t[0] - 1] = 0
+    edge_matrix[edges_t[1] - 1][edges_t[1] - 1] = 0
+    edge_matrix[edges_t[1] - 1][edges_t[0] - 1] = 1
+
+
+def get_path(start, end):
+    # init
+    queue = [start]
+    nodes_seen[start] = 1
+    flag = True
+
+    # BSF algorithm
+    while flag:
+        cur_node = queue.pop(0)
+        print(cur_node)
+        print([edge_matrix[cur_node - 1][i] for i in range(n)])
+
+    return
+
 
 for a0 in range(q):
     u, v = input().strip().split(' ')
     u, v = [int(u), int(v)]
-
-print(edges)
+    get_path(u, v)
