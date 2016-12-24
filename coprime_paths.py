cb36b2b3f7a34ae5d4fc8e584eea0fb57575b58c
nischal <nischal.bhatewara@gmail.com>
2016-12-24 18:05:01 +0530

    Added 0's to matrix

--- coprime_paths.py
+++ coprime_paths.py
@@ -15,8 +15,12 @@ edges = [[-1] * n for i in range(n)]
 print(edges)
 for edges_i in range(n - 1):
     edges_t = [int(edges_temp) for edges_temp in input().strip().split(' ')]
+    # adding vales to edge matrix
     edges[edges_t[0] - 1][edges_t[1] - 1] = 1
+    edges[edges_t[0] - 1][edges_t[0] - 1] = 0
+    edges[edges_t[1] - 1][edges_t[1] - 1] = 0
     edges[edges_t[1] - 1][edges_t[0] - 1] = 1
+
 for a0 in range(q):
     u, v = input().strip().split(' ')
     u, v = [int(u), int(v)]
