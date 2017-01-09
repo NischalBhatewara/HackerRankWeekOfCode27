3b652a35c930d64dd0300c8acb7859b272dea89b
nischal <nischal.bhatewara@gmail.com>
2017-01-09 15:40:15 +0530

    Had to sort the input. HR accepted

--- tailor_shop.py
+++ tailor_shop.py
@@ -1,8 +1,10 @@
+# HR accepted.
+
 import math
 
 n, p = input().strip().split(' ')
 n, p = [int(n), int(p)]
-a = [int(a_temp) for a_temp in input().strip().split(' ')]
+a = [math.ceil(int(a_temp) / p) for a_temp in input().strip().split(' ')]
 
 # button_array = []
 # for cluster_size in clusters:
@@ -10,11 +12,11 @@ a = [int(a_temp) for a_temp in input().strip().split(' ')]
 #         cluster_size = max(button_array) + 1
 #     button_array.append(cluster_size)
 #     tot_buttons += cluster_size
-
+a.sort()
+# print(a)
 tot_buttons = 0
 button_max = 0
-for cluster_cost in a:
-    size = math.ceil(cluster_cost / p)
+for size in a:
     if size <= button_max:
         size = button_max + 1
     button_max, tot_buttons = size, tot_buttons + size
